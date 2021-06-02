e254ace5e6cd39e886f5e21a4a9a6a5a49a73af2
Zain Shahzad <zain.shahzad1801@gmail.com>
2021-06-03 01:13:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 var ArToolkitSource = new THREEx.ArToolkitSource({
-	sourceType: "webcam",
+	sourceType: "image",
 
 })
 
